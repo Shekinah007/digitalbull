f3b407e1b46ede03e02881a17512684986cba5af
Shekinah Shalom-Hope <shalomsheks@gmail.com>
2024-01-30 17:45:19 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { Navbar } from './components/Navbar'
+
+function App() {
+
+  return (
+    <div className="bg-gray-600 w-screen mt-[200px] flex-c">
+      <Navbar />
+      <div className="bg-red-300 h-[400px] w-[300px] w-screen">
+      </div>
+       
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+
+export const Navbar = () => {
+  return (
+    <div className="h-[70px] fixed top-0 w-screen left-0 bg-black flex flex-row justify-between items-center px-5 text-white">    
+        <div className="flex gap-5">
+            <div>
+                <h2>DIGITAL <span className="red">BULL</span></h2>
+                <div className="underline"></div>
+            </div>
+            <div>
+                <a href="#learning-goals">What you will learn</a>
+                <a href="#">Training Results</a>
+                <a href="#course-outline">Course Outline</a>
+            </div>
+        </div>
+        <a href="mailto:digitalbull@gmail.com" className="contact">Contact Us</a>
+    </div>
+  )
+}
